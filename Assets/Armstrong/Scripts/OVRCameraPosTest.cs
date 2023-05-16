77293966c32e8988b331e39a1c9958d5fe4daa9d
almu3988 <almu3988@colorado.edu>
2023-05-16 13:00:26 -0600

    almsot functional 1st person code

--- Assets/Armstrong/Scripts/OVRCameraPosTest.cs
+++ Assets/Armstrong/Scripts/OVRCameraPosTest.cs
@@ -1,4 +1,6 @@
 //Script to test OVRCameraRig and experiment with moving OVRCameraRig.
+// Katy McCutchan
+// May 16 2023
 
 using System.Collections;
 using System.Collections.Generic;
@@ -6,30 +8,33 @@ using UnityEngine;
 
 public class OVRCameraPosTest : MonoBehaviour
 {
-    [SerializeField] private OVRCameraRig overCameraRig; //import state of camera
-    public Vector3 offset = new Vector3(0,1.3f,0); //height offset for Armstrong's eyes
+    [SerializeField] private OVRCameraRig overCameraRig; //initiallize camera
+    public GameObject Armstrong; //initialize armstrong
+    public Vector3 offset = new Vector3(0,0.65f,0); //height offset for Armstrong's eyes
+ 
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 cameraPos = GetCameraPos(); //get position of camera
-        Vector3 amrstrongPos = GetEyePos(); // get armstrong's eye position
-        Debug.Log("Camera Position: " + cameraPos); 
+        Vector3 cameraPos = GetCameraPos(); //get position of camera 
+        Armstrong = GameObject.Find("ZEDM"); //gets ZED mini 
     }
 
+    // Get camera position
     Vector3 GetCameraPos() {
-        //gets camera position
+        
         overCameraRig = GameObject.Find("OVRCameraRig").GetComponent<OVRCameraRig>(); // find postion of OVR Camera Rig
         return overCameraRig.centerEyeAnchor.position; // return postion of OVR Camera Rig
     }
 
-    Vector3 GetEyePos() {
 
 
-    }
-    // Update is called once per frame
+    // Update is called once per frame (use LateUpdate to change camera position after amstrong has moved)
     void Update()
     {
-        
+        Vector3 armstrongPos = Armstrong.transform.position + offset; //get armstrong's eye position, including offset of zed mini
+        Vector3 armstrongRot = Amrstrong.tansform.rotation; //rotation of armstong in world space
+        overCameraRig.transform.position = armstrongPos; //sets camera position to position of zed mini
+        //overCameraRig.transform.rotation = armstrongRot; //set camera rotation to armstrong's rotation
     }
 }
